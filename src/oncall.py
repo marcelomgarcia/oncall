2a49d372d46938ef559b9a60aa0429c5b78d0d8e
Marcelo Garcia <mgarcia@mordor.localdomain>
2020-05-04 21:35:21 +0200

    Read users from file.

--- src/oncall.py
+++ src/oncall.py
@@ -3,7 +3,34 @@
 # Marcelo Garcia
 # May/2020.
 
-if __name__ == "__main__":
-    print("hello")
 
-    
+import configparser as cfgp
+
+def load_users(file_users):
+    """Return a dictionary with the users definition: 
+    name, phone, etc."""
+    users = {}
+    # Create a dictionary with user info.
+    try:
+        config_users = cfgp.ConfigParser()
+        config_users.read(file_users)
+        for ss in config_users.sections():
+            users[ss] = {
+                'name': config_users.get(ss, 'name'), 
+                'phone': config_users.get(ss, 'phone'), 
+                'email': config_users.get(ss, 'email')
+            }
+    except:
+        print("Error loading users from file")
+        # Reset the users.
+        users = {}
+    finally:
+        # Return 'users'. Empty in case of error.
+        return users
+
+
+if __name__ == "__main__":
+    # Load users from file.
+    oc_users = load_users('./files/oncall_people.cfg')
+
+    print(oc_users)
