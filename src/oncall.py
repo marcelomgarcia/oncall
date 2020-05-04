2079c9e433773a7c57867e7313057c51f746338d
Marcelo Garcia <mgarcia@mordor.localdomain>
2020-05-05 00:40:47 +0200

    Added 'add' and 'save' options.

--- src/oncall.py
+++ src/oncall.py
@@ -3,12 +3,13 @@
 # Marcelo Garcia
 # May/2020.
 
-
+import sys
 import configparser as cfgp
+import argparse as argp
 
 def load_users(file_users):
     """Return a dictionary with the users definition: 
-    name, phone, etc."""
+    name, phone, and email."""
     users = {}
     # Create a dictionary with user info.
     try:
@@ -32,5 +33,25 @@ def load_users(file_users):
 if __name__ == "__main__":
     # Load users from file.
     oc_users = load_users('./files/oncall_people.cfg')
+    # print(oc_users)
 
-    print(oc_users)
+    # Create a custom command line depending of the option/cmmand given.
+    if sys.argv[1] == "add":
+        parser = argp.ArgumentParser( 
+            description="Add a new entry to on-call schedule.",
+            usage="oncall.py add user start end")
+        parser.add_argument("add", help="Add entry to on-call schedule")
+        parser.add_argument("-u", "--user", help="Person doing the on-call")
+        parser.add_argument("-s","--start", help="Start of the on-call")
+        parser.add_argument("-e", "--end", help="End of the on-call")
+        args = parser.parse_args()
+    elif sys.argv[1] == "save":
+        parser = argp.ArgumentParser(prog="save", 
+            description="Save HTML file with on-call calendar",
+            usage="oncall.py save --file file_calendar.html")
+        parser.add_argument("save", help="Save HTML file")
+        parser.add_argument("-f", "--file", help="Person doing the on-call")
+        args = parser.parse_args()
+    else:
+        print("noo")
+    print(args)
